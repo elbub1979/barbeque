5a94562ec0567487cb5565a51f7b3ebd5bbffa87
elbub1979 <ilya.leuh09@gmail.com>
2022-05-17 23:35:16 +0300

    add references event <=> user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: URI::MailTo::EMAIL_REGEXP
 end
