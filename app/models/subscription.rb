743d2283439c6b4867485a14c5394eda17c69cc5
elbub1979 <ilya.leuh09@gmail.com>
2022-06-05 02:01:49 +0300

    refactoring validates checking email uniqueness when subscribing

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,16 +2,14 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :event, presence: true
-
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: URI::MailTo::EMAIL_REGEXP, unless: -> { user.present? }
 
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
-  validate :event_creator
 
-  # validate :email_is_use?
+  validate :event_creator
+  validate :unregistered_subscriber_email, unless: -> { user.present? }
 
   # before_validation :user_email_downcase
 
@@ -35,17 +33,17 @@ class Subscription < ApplicationRecord
 
   def event_creator
     if event.user == user
-      errors.add(:event_creator, "activerecord.validates.errors.subscription.event_creator")
+      errors.add(:event_creator, 'activerecord.validates.errors.subscription.event_creator')
     end
   end
 
   # def user_email_downcase
-  #  user_email.downcase!
+  #   user_email.downcase!
   # end
 
-  # def email_is_use?
-  # if User.where('email = ?', user_email.downcase).first
-  #   errors.add(:user_email, 'activerecord.validates.errors.subscription.user_email')
-  #  end
-  # end
+  def unregistered_subscriber_email
+    if User.where('email = ?', user_email).first
+      errors.add(:used, 'activerecord.validates.errors.subscription.user_email')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
 
-  before_save :downcase_email
+  # before_save :downcase_email
 
-  # validates :name, presence: true, length: { maximum: 35 }
-
-  # before_validation :set_name, on: :create
+  validates :name, presence: true, length: { maximum: 35 }
 
   after_commit :link_subscriptions, on: :create
 
   private
 
-  def set_name
-    self.name = "Товарисч №#{rand(777)}" if name.blank?
-  end
-
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: email).update_all(user_id: id)
   end
 
-  def downcase_email
-    email.downcase!
-  end
+  # def downcase_email
+  #   email.downcase!
+  # end
 end
