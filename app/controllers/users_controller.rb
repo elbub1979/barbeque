b90ad7bf83045480451b3f52262bbbf0230b8b31
elbub1979 <ilya.leuh09@gmail.com>
2022-06-05 22:54:53 +0300

    added avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ end
 
 # Only allow a list of trusted parameters through.
 def user_params
-  params.require(:user).permit(:name, :email)
+  params.require(:user).permit(:name, :email, :avatar)
 end
 
