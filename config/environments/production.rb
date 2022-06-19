655691242b033e5bd4a86d3c194b2bc7322053ec
elbub1979 <ilya.leuh09@gmail.com>
2022-06-19 18:37:17 +0300

    refactoring: in production.rb default_url_options in action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'mybarbeque2022@gmail.com' }
+  config.action_mailer.default_url_options = { host: 'myapp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
