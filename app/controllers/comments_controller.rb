998034b6cde32950095298abe56e226e88bf45a2
elbub1979 <ilya.leuh09@gmail.com>
2022-06-21 22:01:49 +0300

    refactoring: comments_controller#notify_subscribers, event_mailer#comments views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
-      notify_subscribers(@event, @new_comment)
+      notify_subscribers(@event)
 
       redirect_to @event, notice: t("controllers.comments.created")
     else
@@ -40,11 +40,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:body, :user_name)
   end
 
-  def notify_subscribers(event, comment)
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [comment.user&.email]).uniq
+  def notify_subscribers(event)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [@new_comment.user&.email]).uniq
 
     all_emails.each do |email|
-      EventMailer.with(comment: comment, email: email).comment.deliver_now
+      EventMailer.with(comment: @new_comment, email: email).comment.deliver_now
     end
   end
 end
