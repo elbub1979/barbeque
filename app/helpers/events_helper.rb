1c5fcfd551da7e0e2e1d38d9d2fb04914eefa8d6
elbub1979 <ilya.leuh09@gmail.com>
2022-06-30 22:14:37 +0300

    refactoring helpers#event_helpers.rb, views/events#show.html.erb

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,10 +3,6 @@ module EventsHelper
     event.user != current_user
   end
 
-  def event_subscriptions
-    @event.subscriptions.except(@new_subscription)
-  end
-
   def not_subscribe?
     @event.visitors.none?(current_user)
   end
