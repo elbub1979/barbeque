2a4b7887843acba0f3f93be65478c3b20b8c8a3b
elbub <ilya.leuh09@gmail.com>
2022-09-08 21:57:00 +0300

    refactoring subscription validates

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,9 +10,9 @@ class Subscription < ApplicationRecord
   with_options unless: :user_present? do
     validates :user_name, presence: true
     validates :user_email, presence: true, format: URI::MailTo::EMAIL_REGEXP
-    validate :event_subscription
-    validate :event_author
-    validate :user_exist
+    validate :subscription_presence
+    validate :event_author_presence
+    validate :user_presence
   end
 
   before_validation :user_email_downcase
@@ -39,16 +39,16 @@ class Subscription < ApplicationRecord
     user_email.downcase! if user_email.present?
   end
 
-  def event_author
+  def event_author_presence
     errors.add(:user_email, :event_creator) if event.user.email == user_email
   end
 
-  def event_subscription
-    errors.add(:user_email, :subscribe_user_email) if event.subscriptions.exists?(user: User.where(email: user_email))
+  def subscription_presence
+    errors.add(:user_email, :subscribe_user_email) if event.subscriptions.exists?(user_email: user_email)
   end
 
-  def user_exist
-    errors.add(:user_email, :present_user) if User.exists?(email: user_email)
+  def user_presence
+    errors.add(:user_email, :user_presence) if User.excluding(event.user).exists?(email: user_email)
   end
 
   def user_present?
